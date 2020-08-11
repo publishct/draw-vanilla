5151bb6a46ea20f5c627fa759570fe608535f45f
Cynthia Trujillo <developercynthia@gmail.com>
2020-08-11 14:09:54 -0500

    functional triangles and color picker with random options for fill and stroke

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,12 @@
 var draw = (function() {
 
-  /* Get the height and width of the main we will use this set canvas to the full
-  size of the main element.*/
+  //Get the height and width of the main we will use this set canvas to the full
+  //size of the main element.
   var mWidth = document.querySelector('main').offsetWidth,
     mHeight = document.querySelector('main').offsetHeight,
 
     //Create the canvas
     canvas = document.createElement("canvas"),
-
-     // set random color
-     color = '#'+Math.floor(Math.random()*16777215).toString(16);
 
     //Create the context
     ctx = canvas.getContext("2d"),
@@ -19,181 +16,259 @@ var draw = (function() {
 
     //current x,y position
     x=0,
-    y=0,
+    y=0;
 
-    //starting x,y
-    x1=0,
-    y1=0,
+  //starting x,y
+  x1=0,
+  y1=0,
 
-    //ending x,y
-    x2=0,
-    y2=0,
+  //ending x,y
+  x2=0,
+  y2=0;
 
-    //Tracks the last x,y state
-    lx = false,
-    ly = false,
+  //Tracks the last x,y state
+  lx = false,
+  ly = false,
 
-    //What shape are we drawing?
-    shape='',
+  //What shape are we drawing?
+  shape='';
 
-    //Are we drawimg a path?
-    isDrawing=false;
+  //Do we want to draw?
+  isDrawing=false;
+
+  //stroke color
+  var stroke='';
+
+  //fill color
+  var fill='';
+
+  //Draw 3-point Triangle
+  var points = [];
+  var i = 0;
 
   return {
+      //Set the x,y coords based on current event data
+      setXY: function(evt) {
 
-    //Set the x,y coords based on current event data
-    setXY: function(evt) {
-
-      //Track last x,y position before setting the current posiiton.
-      lx=x;
-      ly=y;
-
-      //Set the current x,y position
-      x = (evt.clientX - rect.left) - canvas.offsetLeft;
-      y = (evt.clientY - rect.top) - canvas.offsetTop;
-    },
-
+          //Track the last x,y position before setting the current position.
+          lx=x;
+          ly=y;
+      
+          //Set the current x,y position
+          x = (evt.clientX - rect.left) - canvas.offsetLeft;
+          y = (evt.clientY - rect.top) - canvas.offsetTop;
+      },
+  
     //Write the x,y coods to the target div
     writeXY: function() {
       document.getElementById('trackX').innerHTML = 'X: ' + x;
       document.getElementById('trackY').innerHTML = 'Y: ' + y;
     },
 
-    //Set the x1,y1
     setStart: function() {
       x1=x;
       y1=y;
     },
-
-    //Set the x2,y2
+    
     setEnd: function() {
       x2=x;
       y2=y;
     },
 
+  //Draw 3-point Triangle
+  setPoint: function(){
+
+      points[i]=[];
+      points[i]['x']=x;
+      points[i]['y']=y;
+
+      if(points.length>2){
+          this.draw();
+          i=0;
+          points=[];
+      }else{
+          i++;
+      }        
+  },
+    
     //Sets the shape to be drawn
     setShape: function(shp) {
       shape = shp;
     },
 
-        // sets color
-        setColor: function(e) {
-          // find and save element
-          color = e.target.value;
-        },
-
- //A setter for stroke
- setStrokeColor: function(color){
-  stroke = color;
-  },
-
-  //A getter for stroke
-  getStrokeColor: function(){
-    if(stroke.length > 6){
-    return stroke;
-    }
-    return this.randColor();
-  },
-
-  //A setter for fill
-  setFillColor: function(color){
-    fill = color;
-  },
-
-  //A getter for fill
-  getFillColor: function(){
-    if(fill.length > 6){
-    return fill;
-    }
-    return this.randColor();
-  },
-
-    getShape: function() {
-      return shape;
+    //Set a random color
+    randColor: function(){
+      return '#' + Math.floor(Math.random()*16777215).toString(16);
     },
 
+    //A setter for stroke
+    setStrokeColor: function(color){
+    stroke = color;
+    },
+
+    //A getter for stroke
+    getStrokeColor: function(){
+      if(stroke.length > 6){
+      return stroke;
+      }
+      return this.randColor();
+    },
+
+    //A setter for fill
+    setFillColor: function(color){
+      fill = color;
+    },
+
+    //A getter for fill
+    getFillColor: function(){
+      if(fill.length > 6){
+      return fill;
+      }
+      return this.randColor();
+    },
+  
     setIsDrawing: function(bool) {
-      isDrawing = bool;
+    isDrawing = bool;
     },
-
+    
     getIsDrawing: function() {
       return isDrawing;
     },
-
-    //Draws the selected shape
+    
     draw: function() {
       ctx.restore();
       if(shape==='rectangle')
       {
         this.drawRect();
-      } else if( shape==='line' ) {
+      } else if(shape==='line') {
         this.drawLine();
-      } else if( shape==='path' ) {
-        this.drawPath();
-      } else if( shape==='circle' ) {
+      } else if(shape==='circle') {
         this.drawCircle();
-      } else if( shape==='triangle' ) {
-        this.drawTriangle();
+      } else if(shape==='path') {
+        this.drawPath();
+      } else if(shape==='triangle') {
+          this.drawTriangle();
+      } else if(shape==='3-point') {
+          this.draw3Point();
       } else {
-        alert('Please choose a shape');
+      alert('Please choose a shape');
       }
       ctx.save();
     },
 
-    //Draw a circle
-    drawCircle: function() {
-
-      ctx.strokeStyle = color;
-      ctx.fillStyle = color;
-
-      let a = (x1-x2)
-      let b = (y1-y2)
-      let radius = Math.sqrt( a*a + b*b );
-
-      ctx.beginPath();
-      ctx.arc(x1, y1, radius, 0, 2*Math.PI);
+      //Draw a rectangle
+      // drawRect: function(x,y,h,w) {
+      drawRect: function(){
+      ctx.strokeStyle = this.getStrokeColor();
+      //Start by using random fill colors.
+      ctx.fillStyle = this.getFillColor();
+      // ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);      
+      ctx.fillRect (x1,y1,(x2-x1),(y2-y1));
       ctx.stroke();
-      ctx.fill();
     },
 
-    //Draw a line
-    drawLine: function() {
+      //Draw a line
+      drawLine: function() {
       //Start by using random fill colors.
-      ctx.strokeStyle = color;
+      // ctx.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+      ctx.strokeStyle = this.getStrokeColor();
       ctx.beginPath();
       ctx.moveTo(x1, y1);
       ctx.lineTo(x2, y2);
       ctx.stroke();
     },
 
+      //Draw a circle
+      drawCircle: function() {
+          ctx.strokeStyle = this.getStrokeColor();
+          // ctx.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+          ctx.fillStyle = this.getFillColor();
+          // ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+      
+          let a = (x1-x2)
+          let b = (y1-y2)
+          let radius = Math.sqrt( a*a + b*b );
+      
+          ctx.beginPath();
+          ctx.arc(x1, y1, radius, 0, 2*Math.PI);
+          ctx.stroke();
+          ctx.fill();
+      },
 
-    drawPath: function() {
-      //console.log({x1:x,y1:y,x2:x2,y2:y2});
-      //Start by using random fill colors.
-      ctx.strokeStyle = color;
-      ctx.beginPath();
-      ctx.moveTo(lx, ly);
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    },
-
-    //Draw a rectangle
-    drawRect: function() {
-      //Start by using random fill colors.
-      ctx.fillStyle = color;
-      ctx.fillRect (x1,y1,(x2-x1),(y2-y1));
-    },
+      //Draw a path
+      drawPath: function() {
+          //Start by using random fill colors.
+          ctx.strokeStyle = this.getStrokeColor();
+          // ctx.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+          ctx.beginPath();
+          ctx.moveTo(lx, ly);
+          ctx.lineTo(x, y);
+          ctx.stroke();
+      },
 
     //Draw a triangle
-    drawTriangle: function() {
-      //Start by using random fill colors.
-      ctx.fillStyle = color;
-      ctx.fillTriangle ((x2-x1),y1,(Math.sqrt(Math.pow((x2-x1), 2) + Math.pow(y1, 2))));
-    },
+    drawTriangle: function(){
 
-    getCanvas: function(){
-      return canvas;
-    },
+      //x1,y1 to x2,y2 is the first line we will use the first point +/- 
+      //(depending on the direction of the mouse movement) the result of 
+      //PT to add a third point. 
+      var a = (x1-x2);
+      var b = (y1-y2);
+      var c = Math.sqrt(a*a + b*b);
+
+      var d = x1+c;
+      var e = y1+c;
+
+      //Drag left to right
+      if(x1>x2){
+          d=x1-c;
+      }
+
+      //Drag up
+      if(y1>y2){
+          e=y1-c;
+      }
+  
+      ctx.fillStyle = this.getFillColor();
+      ctx.strokeStyle = this.getStrokeColor();
+      ctx.beginPath();
+      ctx.moveTo(x1, y1);
+
+      ctx.lineTo(d,e);
+      ctx.lineTo(x2, y2);
+
+      ctx.lineTo(x1, y1);
+      ctx.stroke();
+      ctx.fill();
+  },
+
+      //Draw 3-point Triangle
+      draw3Point: function(){
+
+      ctx.fillStyle = this.getFillColor();
+      ctx.strokeStyle = this.getStrokeColor();
+
+      ctx.beginPath();
+
+      ctx.moveTo(points[0]['x'], points[0]['y']);
+      ctx.lineTo(points[1]['x'], points[1]['y']);
+      ctx.lineTo(points[2]['x'], points[2]['y']);
+      ctx.lineTo(points[0]['x'], points[0]['y']);
+
+      ctx.stroke();
+      ctx.fill();
+  },
+
+      getShape: function() {
+      return shape;
+      },
+                
+      getCanvas: function(){
+          return canvas;
+      },
+
+   
+
 
     //Initialize the object, this must be called before anything else
     init: function() {
@@ -209,6 +284,30 @@ var draw = (function() {
 //Initialize draw
 draw.init();
 
+  document.getElementById('btnRect').addEventListener('click',function(){
+  draw.setShape('rectangle');
+  }, false);
+
+  document.getElementById('btnLine').addEventListener('click',function(){
+      draw.setShape('line');
+  }, false);
+
+  document.getElementById('btnCircle').addEventListener('click',function(){
+      draw.setShape('circle');
+  }, false);
+
+  document.getElementById('btnPath').addEventListener('click',function(){
+      draw.setShape('path');
+  }, false);
+
+  document.getElementById('btnTriangle').addEventListener('click',function(){
+      draw.setShape('triangle');
+  }, false);
+
+  document.getElementById('btn3Point').addEventListener('click', function(){
+      draw.setShape('3-point');
+    });
+    
 //Add a mousemove listener to the canvas
 //When the mouse reports a change of position use the event data to
 //set and report the x,y position on the mouse.
@@ -219,42 +318,42 @@ draw.getCanvas().addEventListener('mousemove', function(evt) {
     draw.draw();
   }
 }, false);
-
-//Add a mousedown listener to the canvas
+    
 //Set the starting position
 draw.getCanvas().addEventListener('mousedown', function() {
+  //Draw 3-point Triangle
+  if(draw.getShape()!=='3-point'){
   draw.setStart();
   draw.setIsDrawing(true);
+  }
 }, false);
 
-//Add a mouseup listener to the canvas
-//Set the end position and draw the rectangle
 draw.getCanvas().addEventListener('mouseup', function() {
+  //Draw 3-point Triangle
+  if(draw.getShape()!=='3-point'){
   draw.setEnd();
   draw.draw();
   draw.setIsDrawing(false);
+  }
+  //Draw 3-point Triangle
+  if(draw.getShape()==='3-point'){
+  draw.setPoint();
+  }  
 }, false);
 
-document.getElementById('btnRect').addEventListener('click', function(){
-    draw.setShape('rectangle');
-}, false);
+document.getElementById('strokeColor').addEventListener('change', function(){
+  draw.setStrokeColor(document.getElementById('strokeColor').value);
+});
 
-document.getElementById('btnLine').addEventListener('click', function(){
-    draw.setShape('line');
-}, false);
+document.getElementById('randStrokeColor').addEventListener('change', function(){
+draw.setStrokeColor('');
+});
 
-document.getElementById('btnCircle').addEventListener('click', function(){
-    draw.setShape('circle');
-}, false);
-
-document.getElementById('btnPath').addEventListener('click', function(){
-    draw.setShape('path');
-}, false);
-
-document.getElementById('btnTriangle').addEventListener('click', function(){
-  draw.setShape('triangle');
-}, false);
-
-document.getElementById('favColor').addEventListener('change', function() {
-  draw.setColor(color)
-})
+document.getElementById('fillColor').addEventListener('change', function(){
+draw.setFillColor(document.getElementById('fillColor').value);
+});
+  
+document.getElementById('randFillColor').addEventListener('change', function(){
+draw.setFillColor('');
+});
+    
